69c65b3e9143be821f9f538b9d277f22ad80b193
ami <ami.malimovka@gmail.com>
2015-05-20 10:30:43 +0300

    added css file + can now control the elements classes from the object init

--- minuteWidget.jquery.js
+++ minuteWidget.jquery.js
@@ -59,9 +59,9 @@
 			
 			function buildTheDOM(minutesInput, minutesVal, minutesSlider, minutesLabel, hoursVal, hoursSlider, hoursLabel) {
 				minutesInput.hide();
-				var widget = $("<div class='minuteSlidersWidget'></div>");
-				var minutesContainer = $("<div class='minutesContainer'></div>");
-				var hoursContainer = $("<div class='hoursContainer'></div>");
+				var widget = $("<div class='"+writeClassAttr("widgetClass")+"'></div>");
+				var minutesContainer = $("<div class='"+writeClassAttr("minutesContainerClass")+"'></div>");
+				var hoursContainer = $("<div class='"+writeClassAttr("hoursContainerClass")+"'></div>");
 				minutesContainer.append(minutesVal);
 				minutesContainer.append(minutesSlider);
 				minutesContainer.append(minutesLabel);
@@ -72,6 +72,19 @@
 				widget.append(hoursContainer);
 				minutesInput.after(widget);
 				updateSlidersFromInput(minutesInput, minutesSlider, hoursSlider, minutesVal, hoursVal);
+			}
+			
+			function writeClassAttr(data) {
+				if(undefined === data) {
+					return "";
+				}
+				if(undefined !== settings[data]) {
+					data = settings[data];
+				}
+				if(data instanceof Array) {
+					return $.map(data, function(s) {return String(s);}).join(" ");
+				}
+				return String(data);
 			}
 			
 			function hookEvents(minutesInput, minutesVal, minutesSlider, minutesLabel, hoursVal, hoursSlider, hoursLabel) {
@@ -95,8 +108,15 @@
 			
 			var settings = $.extend({
 				"debug": false,
+				"widgetClass": "minuteSlidersWidget",
+				"minutesContainerClass": ["minutesContainer", "form-group"],
+				"hoursContainerClass": ["hoursContainer", "form-group"],
 				"hoursLabel": "hours",
 				"minutesLabel": "minutes",
+				"labelClass": "control-label",
+				"sliderClass": "form-control",
+				"hoursValueClass": "",
+				"minutesValueClass": "",
 				"onChange": function (o) {
 					if(true === settings.debug) {
 						console.log(o);
@@ -109,13 +129,13 @@
 				var elId = minutesInput.prop("id");
 				var elName = minutesInput.prop("name");
 				
-				var minutesSlider = $('<input id="minutes_' + elName + '" type="range" min="0" max="59" value="0"/>');
-				var minutesLabel = $('<label class="control-label" for="minutes_' + elName + '">' + settings.minutesLabel + '</label>');
-				var minutesVal = $('<span id="minutesVal_' + elName + '">0</span>');
+				var minutesSlider = $('<input id="minutes_' + elName + '" type="range" min="0" max="59" value="0" class="'+writeClassAttr("sliderClass")+'"/>');
+				var minutesLabel = $('<label class="'+writeClassAttr("labelClass")+'" for="minutes_' + elName + '">' + settings.minutesLabel + '</label>');
+				var minutesVal = $('<span id="minutesVal_' + elName + '" class="'+writeClassAttr("minutesValueClass")+'">0</span>');
 				
-				var hoursSlider = $('<input id="hours_' + elName + '" type="range" min="0" max="24" value="0"/>');
-				var hoursLabel = $('<label class="control-label" for="hours_' + elName + '">' + settings.hoursLabel + '</label>');
-				var hoursVal = $('<span id="hoursVal_' + elName + '">0</span>');
+				var hoursSlider = $('<input id="hours_' + elName + '" type="range" min="0" max="24" value="0" class="'+writeClassAttr("sliderClass")+'"/>');
+				var hoursLabel = $('<label class="'+writeClassAttr("labelClass")+'" for="hours_' + elName + '">' + settings.hoursLabel + '</label>');
+				var hoursVal = $('<span id="hoursVal_' + elName + '" class="'+writeClassAttr("hoursValueClass")+'">0</span>');
 								
 				buildTheDOM(minutesInput, minutesVal, minutesSlider, minutesLabel, hoursVal, hoursSlider, hoursLabel);
 				
